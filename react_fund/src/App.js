062abab0b25f1eafa420496124b021284aaa5a54
bardzim <bardzim@gmail.com>
2022-09-07 20:37:23 +0400

    working with classes and components

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -1,30 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
 
 
 function App() {
-  const [likes , setLikes] = useState(0);
+
   const [value, setValue] = useState('input text')
 
-  function increment() {
-    setLikes(likes + 1)
-  }
-  
-  function decrement() {
-    setLikes(likes - 1)
-  }
-
   return (
-    <div>
-    <h1>{likes}</h1>
-    <h1>{value}</h1>
-      <input 
-      type="text" 
-      value={value}
-      onChange={e => setValue(e.target.value)}
-      />
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button> 
+    <div  className='App'>
+      {/* <Counter/> */}
+      <ClassCounter/>
     </div>
   );
 }
