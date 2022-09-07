4cbb8e6de7566fa735c603574e5cb3be7bcec52a
bardzim <bardzim@gmail.com>
2022-09-07 13:00:03 +0400

    react hooks {useState}

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [likes , setLikes] = useState(0);
+
+  function increment() {
+    setLikes(likes + 1)
+  }
+  
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div>
+    <h1>{likes}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button> 
     </div>
   );
 }
