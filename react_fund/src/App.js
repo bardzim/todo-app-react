c19b1d9e9d47769deec7df1f955f544a161a7ccd
bardzim <bardzim@gmail.com>
2022-09-07 13:17:30 +0400

    chain DOM elements with hooks

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [likes , setLikes] = useState(0);
+  const [value, setValue] = useState('input text')
 
   function increment() {
     setLikes(likes + 1)
@@ -16,6 +17,12 @@ function App() {
   return (
     <div>
     <h1>{likes}</h1>
+    <h1>{value}</h1>
+      <input 
+      type="text" 
+      value={value}
+      onChange={e => setValue(e.target.value)}
+      />
       <button onClick={increment}>increment</button>
       <button onClick={decrement}>decrement</button> 
     </div>
