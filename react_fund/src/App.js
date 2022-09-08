29fc99671e1a065efc6bcb951aac2f3953406ed0
bardzim <bardzim@gmail.com>
2022-09-08 21:15:43 +0400

    create UI lib. css Modules children Props

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 
@@ -10,16 +12,30 @@ const [posts, setPosts] = useState([
   {id: 2, title: '2. JavaScript', body: 'Description'},
   {id: 3, title: '3. JavaScript', body: 'Description'}
 ])
-const [posts2, setPosts2] = useState([
-  {id: 1, title: '1. Python', body: 'Description'},
-  {id: 2, title: '2. Python', body: 'Description'},
-  {id: 3, title: '3. Pithon', body: 'Description'}
-])
+
+const [title, setTitle] = useState('');
+
+const addNewPost = (e) => {
+  e.preventDefault()
+  console.log(title)
+}
 
   return (
     <div className='App'>
+      <form>
+        <div>
+          {/*Managable Component*/}
+          <MyInput 
+          value={title}
+          onChange= {e => setTitle(e.target.value)}
+          type='text' 
+          placeholder='Post name'
+          />
+          <MyInput type='text' placeholder='Post description'/>
+          <MyButton onClick={addNewPost}>create post</MyButton>
+        </div>
+      </form>
       <PostList posts={posts} title='Post List 1'/>
-      <PostList posts={posts2} title='Post List 2'/>
     </div>
   );
 }
