9748c4de4a35dc8f061cd74eb2ce65d2295346da
bardzim <bardzim@gmail.com>
2022-09-08 10:45:02 +0400

    working with Props

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -9,9 +9,7 @@ function App() {
 
   return (
     <div className='App'>
-      <PostItem/>
-      <PostItem/>
-      <PostItem/>
+      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
     </div>
   );
 }
