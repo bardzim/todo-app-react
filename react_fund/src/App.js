04ad431838fbc391756f1b873c6b8b97ccfecde9
bardzim <bardzim@gmail.com>
2022-09-16 13:47:02 +0400

    get posts with axxios and TransitionGroup

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -6,15 +6,14 @@ import './styles/App.css';
 import MyModal from './components/UI/Modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
+import axios from 'axios';
 
 
 
 function App() {
 
 const [posts, setPosts] = useState([
-  {id: 1, title: 'aa', body: 'cc'},
-  {id: 2, title: 'bb', body: 'bb'},
-  {id: 3, title: 'cc', body: 'aa'},
+
 ])
 
 const [filter, setFilter] = useState({sort:'', query:''})
@@ -34,8 +33,18 @@ const removePost = (post) => {
   setPosts(posts.filter(p => p.id !== post.id))
 }
 
+async function fetchPosts() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+  setPosts(response.data)
+}
+
   return (
     <div className='App'>
+      <MyButton
+        onClick={fetchPosts}
+      >
+        GET POSTS
+      </MyButton>
       <MyButton
         style={{marginTop: 30}}
         onClick={() => setModal(true)}
