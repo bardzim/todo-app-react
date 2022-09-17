ba63a8b487e53f9a2deb0e589ffbd668bb75bd1c
bardzim <bardzim@gmail.com>
2022-09-17 19:26:23 +0400

    custom useFetching hook

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -8,6 +8,7 @@ import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
 
 
 
@@ -17,7 +18,11 @@ const [posts, setPosts] = useState([])
 const [filter, setFilter] = useState({sort:'', query:''})
 const [modal, setModal] = useState(false)
 const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-const [isPostsLoading, setIsPostsLoading] = useState(false)
+const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
+  const posts = await PostService.getAll();
+  setPosts(posts)
+})
+
 /* const [title, setTitle] = useState('');
 const [subTitle, setSubTitle] = useState(''); */
 
@@ -34,15 +39,6 @@ const createPost = (newPost) => {
 //get Post From Child Component
 const removePost = (post) => {
   setPosts(posts.filter(p => p.id !== post.id))
-}
-
-async function fetchPosts() {
-    setIsPostsLoading(true)
-    setTimeout( async() => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostsLoading(false)
-    }, 1000)
 }
 
   return (
@@ -68,7 +64,10 @@ async function fetchPosts() {
       <PostFilter
         filter={filter}
         setFilter={setFilter}
-      /> 
+      />
+      { postError &&
+        <h1>Error loading ${postError}</h1>
+      } 
       { isPostsLoading
         ? <div style={{display: 'flex',justifyContent: 'center', marginTop: 50}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JavaScript Posts"/>
---
+++ react_fund/src/hooks/useFetching.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const useFetching = (callback) => {
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    const fetching = async () => {
+        try {
+            setIsPostsLoading(true)
+            await (callback)
+        } catch(e) {
+            setError(e.message)
+        } finally {
+            setIsPostsLoading(false)
+            await (callback)
+        }
+    }
+    return [fetching, isPostsLoading,error]
+}
+
