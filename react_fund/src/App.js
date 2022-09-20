d467c17c6177880a6cb389d7bad746400336121e
bardzim <bardzim@gmail.com>
2022-09-20 21:06:32 +0400

    useFetching cerror processing

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -6,7 +6,7 @@ import './styles/App.css';
 import MyModal from './components/UI/Modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
-import PostService from './API/PostService';
+import  PostService  from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
 
@@ -18,20 +18,17 @@ const [posts, setPosts] = useState([])
 const [filter, setFilter] = useState({sort:'', query:''})
 const [modal, setModal] = useState(false)
 const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-
-
-const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-  const posts = await PostService.getAll()
+const [fetchPosts, isLoading, postError ] = useFetching (async() => {
+  const posts = await PostService.getAll();
   setPosts(posts)
 })
-
 /* const [title, setTitle] = useState('');
 const [subTitle, setSubTitle] = useState(''); */
 
 //this useEffect will work once because array don't have any deps
 useEffect(()=> {
   fetchPosts();
-},)
+},[])
 
 const createPost = (newPost) => {
   setPosts([...posts, newPost])
@@ -67,13 +64,14 @@ const removePost = (post) => {
         filter={filter}
         setFilter={setFilter}
       />
-      { postError &&
-        <h1>Error loading ${postError}</h1>
-      } 
-      { isPostsLoading
-        ? <div style={{display: 'flex',justifyContent: 'center', marginTop: 50}}><Loader/></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JavaScript Posts"/>
-      }
+      {postError &&
+            <h1>Error ${postError}</h1>
+        }
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+            <div style={{height: 20, background: 'red'}}/>
+            {isLoading &&
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+            }
     </div>
   );
 }
--- react_fund/src/hooks/useFetching.js
+++ react_fund/src/hooks/useFetching.js
@@ -1,20 +1,18 @@
-import { useState } from "react"
+import {useState} from "react";
 
 export const useFetching = (callback) => {
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
-    const [error, setError] = useState('')
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
 
     const fetching = async () => {
         try {
-            setIsPostsLoading(true)
-            await (callback)
-        } catch(e) {
+            setIsLoading(true)
+            await callback()
+        } catch (e) {
             setError(e.message)
         } finally {
-            setIsPostsLoading(false)
-            await (callback)
+            setIsLoading(false)
         }
     }
-    return [fetching, isPostsLoading,error]
+    return [fetching, isLoading, error]
 }
-
