f6f737852cc2f15a5ea9afd5eeca98f0a7a4db2b
bardzim <bardzim@gmail.com>
2022-09-21 12:02:46 +0400

    page pagination

--- react_fund/src/App.js
+++ react_fund/src/App.js
@@ -9,6 +9,7 @@ import { usePosts } from './hooks/usePosts';
 import  PostService  from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 
 
@@ -17,11 +18,21 @@ function App() {
 const [posts, setPosts] = useState([])
 const [filter, setFilter] = useState({sort:'', query:''})
 const [modal, setModal] = useState(false)
+const [totalPages, setTotalPages] = useState(0)
+const [limit, setLimit] = useState(10)
+const [page, setPage] = useState(1)
 const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+const pagesArray = getPagesArray(totalPages)
+
+
 const [fetchPosts, isLoading, postError ] = useFetching (async() => {
-  const posts = await PostService.getAll();
-  setPosts(posts)
+  const response = await PostService.getAll(limit, page);
+  setPosts(response.data)
+  const totalCount = response.headers['x-total-count']
+  setTotalPages(getPageCount(totalCount, limit))
 })
+console.log(pagesArray)
+console.log(totalPages)
 /* const [title, setTitle] = useState('');
 const [subTitle, setSubTitle] = useState(''); */
 
@@ -38,6 +49,11 @@ const createPost = (newPost) => {
 //get Post From Child Component
 const removePost = (post) => {
   setPosts(posts.filter(p => p.id !== post.id))
+}
+
+const changePage = (page) => {
+  setPage(page)
+  fetchPosts();
 }
 
   return (
@@ -67,11 +83,19 @@ const removePost = (post) => {
       {postError &&
             <h1>Error ${postError}</h1>
         }
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JavaScript Posts"/>
             <div style={{height: 20, background: 'red'}}/>
             {isLoading &&
             <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-            }
+        }
+        <div className='page__wrapper'>
+            {pagesArray.map(p =>
+              <span 
+                    onClick={() => changePage(p)}
+                    key={p} 
+                    className={page === p ? 'page page__current' : 'page'}>{p}</span>
+            )}
+        </div>
     </div>
   );
 }
